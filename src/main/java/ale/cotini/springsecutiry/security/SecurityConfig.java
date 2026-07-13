84e7bf94de74007b3e3005cf9243c70034ed8307
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-13 10:49:16 +0200

    create SecurityConfig

---
+++ src/main/java/ale/cotini/springsecutiry/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ale.cotini.springsecutiry.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        // DISABILITO IL FORM DI DEFAULT
+        
+        httpSecurity.formLogin(formLogin -> formLogin.disable() );
+
+        //ELIMINO I CONTROLLI AUTOMATICI DI SPRING SECURITY
+
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+        //IMPLEMENTO I MIEI CONTROLLI BASATI SUI TOKEN
+
+        return httpSecurity.build();
+
+    }
+}
